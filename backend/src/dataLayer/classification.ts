2b5075996aaebc44a1e10328fb870a7775f10d38
Blahut, György <fable3@gmail.com>
2021-05-24 00:40:39 +0200

    classifier S3 deployment testing

--- backend/src/dataLayer/classification.ts
+++ backend/src/dataLayer/classification.ts
@@ -6,12 +6,14 @@ const logger = createLogger('classification')
 var lambda = new AWS.Lambda();
 
 export async function getClassLabel(key:string) {
+    //const stage = process.env.STAGE
     var params = {
-      FunctionName: "classifiertest", 
+      //FunctionName: `serverless-udagram-app-${stage}-classifier`, 
+      FunctionName: `classifiertest`, 
       Payload: JSON.stringify({key})
     };
     
-    logger.info("getClassLabel", {key});
+    logger.info("getClassLabel", params);
     const response = await lambda.invoke(params).promise();
     logger.info("returned", {response});
     const payload = response.Payload as string
